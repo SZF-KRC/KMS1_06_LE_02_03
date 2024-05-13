65015441a08721a51699e0376722080994842df4
Jaroslav Krc <jaroslav.krc@gmail.com>
2024-05-13 18:54:03 +0200

    Projektdateien hinzufügen.

---
+++ KMS1_06_LE_02_03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMS1_06_LE_02_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
